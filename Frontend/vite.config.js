d8ece7c21e847a7d9ac345c2b4b87448b2b0de14
aleksanderboe <aleksanderbo10@gmail.com>
2025-05-04 19:04:57 +0200

    try again

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+
   base: '/', // Set to '/subpath/' if deployed in a subdirectory
   resolve: {
     alias: {
